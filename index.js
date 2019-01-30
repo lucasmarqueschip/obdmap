093b883b042815f8c314eb9499f83f844b5c76f1
lucasmarqueschip <47149887+lucasmarqueschip@users.noreply.github.com>
2019-01-30 15:23:29 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var divBtn = document.getElementById('btnDiv')
 
 function btnConnectClick() {
   let nameDevice = document.querySelector('#name').value;
-  div.innerHTML += 'Botao pressionado<br/>'
   // Nome do dispositivo para filtrar, div para logs e botao para habilitar no fim da promisse
   ConnectBluetooth(nameDevice)
 }
