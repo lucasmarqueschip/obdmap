e347e411ec2a5488766e00d58cb39f197606b498
lucasmarqueschip <47149887+lucasmarqueschip@users.noreply.github.com>
2019-01-31 09:50:28 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var log = document.getElementById('logs')
 var divBtn = document.getElementById('btnDiv')
 var vs = document.getElementById('VersionBluetooth')
+var btnConnect = document.getElementById('btnConnect')
+var btnDisconnect = document.getElementById('btnDisConnect')
+var btnReconnect = document.getElementById('btnReconnect')
+
 function btnConnectClick() {
   let nameDevice = document.querySelector('#name').value;
   // Nome do dispositivo para filtrar, div para logs e botao para habilitar no fim da promisse
-  ConnectBluetooth(nameDevice)
+  ConnectBluetooth(nameDevice, 'btn', [btnDisconnect, btnReconnect])
+}
+
+function btnDisconnectClick() {
+  DisconnectBluetooth([btnReconnect], 'btn')
+}
+
+function btnReconnectClick(){
+  
+  ReconnectBluetooth([btnDisconnect], 'btn')
 }
 
 function btnReadServiceClick() {
-
+  
 }
 
 
